842c1520ddddd04a8b0da5c641749382682ce251
andersonZNLT <anderson.znlt@gmail.com>
2022-06-14 21:41:43 -0300

    limite de contagem entre 0 a 10

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,13 @@ var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0;
 
 function increment() {
+    if (currentNumber >= 10) return;
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
+    if (currentNumber <= 0) return;
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
