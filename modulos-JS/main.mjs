4db01b8c667e87235cfb13a09e46356e4a7350d9
Anderson <engenharia@hdeletro.com.br>
2022-06-22 13:01:52 -0300

    módulos em java script

---
+++ modulos-JS/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Anderson', 36));
+console.log(mostraCidade('Anderson', 'Criciúma'));
+console.log(mostraHobby('Anderson', 'Soldar'));
